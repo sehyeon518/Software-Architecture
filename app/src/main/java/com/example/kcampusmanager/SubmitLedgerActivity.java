a2a75dbf4e349dd2d8dc52094102f5651bee4826
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2023-11-30 17:08:03 +0900

    feat: 대여 신청 날짜 calendar 선택 기능 추가

--- app/src/main/java/com/example/kcampusmanager/SubmitLedgerActivity.java
+++ app/src/main/java/com/example/kcampusmanager/SubmitLedgerActivity.java
@@ -2,24 +2,29 @@ package com.example.kcampusmanager;
 
 import static java.lang.String.valueOf;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+
 public class SubmitLedgerActivity extends AppCompatActivity {
 
-    ImageView back;
-    TextView studentNumber, submit;
+    ImageView back, calendar;
+    TextView studentNumber, borrowDate, submit;
     Spinner spinner;
 
     int userNumber;
+    DatePickerDialog datePickerDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +37,8 @@ public class SubmitLedgerActivity extends AppCompatActivity {
         back = findViewById(R.id.back);
         studentNumber = findViewById(R.id.userNumber);
         spinner = findViewById(R.id.classroom_spinner);
+        borrowDate = findViewById(R.id.borrowDate);
+        calendar = findViewById(R.id.calendar);
         submit = findViewById(R.id.submit);
 
         back.setOnClickListener(v -> {
@@ -47,8 +54,21 @@ public class SubmitLedgerActivity extends AppCompatActivity {
         submit.setOnClickListener(v -> {
             finish();
         });
-    }
 
+        calendar.setOnClickListener(view -> {
+            Calendar cal = Calendar.getInstance();
+            int pYear = cal.get(Calendar.YEAR);
+            int pMonth = cal.get(Calendar.MONTH);
+            int pDay = cal.get(Calendar.DAY_OF_MONTH);
+
+            datePickerDialog = new DatePickerDialog(SubmitLedgerActivity.this, (datePicker, year, month, day) -> {
+                month = month + 1;
+                String date = year + "/" + month + "/" + day;
+                borrowDate.setText(date);
+            }, pYear, pMonth, pDay);
+            datePickerDialog.show();
+        });
+    }
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
         // An item was selected. You can retrieve the selected item using
         // parent.getItemAtPosition(pos)
