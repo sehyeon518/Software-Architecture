805330c6a921537c0191d6bb931b9d2896ceedec
Eonji-sw <ejkim0625@gmail.com>
2023-12-12 19:39:48 +0900

    feat: 강의실 시간표 spinner 연결

--- app/src/main/java/com/example/kcampusmanager/ClassScheduleActivity.java
+++ app/src/main/java/com/example/kcampusmanager/ClassScheduleActivity.java
@@ -15,6 +15,7 @@ public class ClassScheduleActivity extends AppCompatActivity {
     ImageView back;
     Spinner spinner;
     LinearLayout swap;
+    ImageView schedule;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,20 +32,46 @@ public class ClassScheduleActivity extends AppCompatActivity {
             ; // TODO 헬퍼 시간표 swap
         });
 
-        spinner = (Spinner) findViewById(R.id.classroom_spinner);
+        spinner = findViewById(R.id.classroom_spinner);
+        schedule = findViewById(R.id.iv_classroom_schedule);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.classroom_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinnerListener();
     }
 
-    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        // An item was selected. You can retrieve the selected item using
-        // parent.getItemAtPosition(pos)
-    }
+    public void spinnerListener() {
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        schedule.setImageResource(R.drawable.img_schedule_classroom_231);
+                        break;
+                    case 1:
+                        schedule.setImageResource(R.drawable.img_schedule_classroom_232);
+                        break;
+                    case 2:
+                        schedule.setImageResource(R.drawable.img_schedule_classroom_424);
+                        break;
+                    case 3:
+                        schedule.setImageResource(R.drawable.img_schedule_classroom_445);
+                        break;
+                    case 4:
+                        schedule.setImageResource(R.drawable.img_schedule_classroom_447);
+                        break;
+                    case 5:
+                        schedule.setImageResource(R.drawable.img_schedule_classroom_611);
+                        break;
+                }
+            }
 
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
